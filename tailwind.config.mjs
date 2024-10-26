199d3d087ba357aee6b8e39536dae2a0886d6079
federicosldev <sanchezlisonfederico2001@gmail.com>
2024-10-26 22:01:26 +0200

    New sections and images

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,19 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary-purple': '#6976DF',
+				'primary-pink': '#EFA6F5',
+				'light-purple': '#BBC5F7',
+				'dark-purple': '#5A65DC',
+			  },
+			backgroundImage: {
+				'lightpurple-pattern': "url('/public/fondo-lila.svg')",
+			  }
+		},
 	},
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/typography')
+	],
 }
