f5c93f323fa2efe0f04a47cad8bd57586ed69cbb
federicosldev <sanchezlisonfederico2001@gmail.com>
2024-11-23 23:48:59 +0100

    Hero section redesing

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,11 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				'title': ['Monigue'],
+				'paragrahp': ['Cssglykeregular'],
+				'banner': ['Menda']
+			},
 			colors: {
 				'primary-purple': '#6976DF',
 				'primary-pink': '#EFA6F5',
