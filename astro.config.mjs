1b299a2fa9e9f88e9303a2c5938fc3d8a499d9b0
federicosldev <sanchezlisonfederico2001@gmail.com>
2024-12-09 22:17:55 +0100

    Deploy error resolve

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/static';  // or 'serverless' if you need SSR
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: 'static',          // or 'server' if you need SSR
+  adapter: vercel(),
 });
