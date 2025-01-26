6d66e7b6e4256b06a33c8715f5d190ff9a8e240e
federicosldev <sanchezlisonfederico2001@gmail.com>
2025-01-26 19:52:39 +0100

    fix animation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/static';  // or 'serverless' if you need SSR
 
+
 export default defineConfig({
   integrations: [tailwind()],
   output: 'static',          // or 'server' if you need SSR
@@ -9,6 +10,9 @@ export default defineConfig({
   vite: {
     ssr: {
       noExternal: ['matter-js'],
+    },
+    optimizeDeps: {
+      include: ['matter-js'],
     },
   },
 });
