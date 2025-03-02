f924493c8cde4dc0667338f6fc68989e53c803fd
federicosldev <sanchezlisonfederico2001@gmail.com>
2025-03-02 20:57:51 +0100

    Project fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,22 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/static';  // or 'serverless' if you need SSR
-
-
 import react from '@astrojs/react';
 
-
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    
+  ],
   output: 'static',          // or 'server' if you need SSR
   adapter: vercel(),
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp',
+    },
+    remotePatterns: [{ protocol: 'https' }],
+    domains: [],
+  },
   vite: {
     ssr: {
       noExternal: ['matter-js'],
