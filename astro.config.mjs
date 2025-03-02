f08b0254a4853582597741830834ba48844a194a
federicosldev <sanchezlisonfederico2001@gmail.com>
2025-03-02 21:07:50 +0100

    Fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/static';  
+import vercel from '@astrojs/vercel/static';  // or 'serverless' if you need SSR
+
+
 import react from '@astrojs/react';
 
+
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    
-  ],
-  output: 'static',          
+  integrations: [tailwind()],
+  output: 'static',          // or 'server' if you need SSR
   adapter: vercel(),
   image: {
     service: {
