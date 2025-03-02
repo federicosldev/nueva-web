122a46a335f936a506012bc1b41cb0edf95414b1
federicosldev <sanchezlisonfederico2001@gmail.com>
2025-03-02 21:10:04 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,13 +10,6 @@ export default defineConfig({
   integrations: [tailwind()],
   output: 'static',          // or 'server' if you need SSR
   adapter: vercel(),
-  image: {
-    service: {
-      entrypoint: 'astro/assets/services/sharp',
-    },
-    remotePatterns: [{ protocol: 'https' }],
-    domains: [],
-  },
   vite: {
     ssr: {
       noExternal: ['matter-js'],
